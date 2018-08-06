766cff63dd0d79d4e68d515f3b1b073b753bac13
sumarsono <sumarsono.wongbandar@gmail.com>
2018-08-06 19:12:57 +0700

    Update wireless_controlles_power_outlet.ino
    add vars

--- wireless_controlles_power_outlet.ino
+++ wireless_controlles_power_outlet.ino
@@ -1,3 +1,27 @@
+#include <Pushbutton.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>
+#include <FS.h>
+#include <ArduinoOTA.h>
+
+ESP8266WiFiMulti wifiMulti;
+
+//soft AP
+const char *ssid = "SSID";
+const char *password = "PASSWD";
+
+ESP8266WebServer server(80);
+
+String getContentType(String filename);
+bool handleFileRead(String path);
+
+const int pinTombol = D5;
+const int pinLampu = D6;
+const int pinKipas = D7;
+
 void setup {
 }
 
